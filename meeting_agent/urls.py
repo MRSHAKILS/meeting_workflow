7848b2b64d171e50481f39841884b0010beeab82
alvi00 <ahmadfahmid59@gmail.com>
2025-06-18 11:04:19 +0600

    removed hardcoded OAuth keys, using env vars , added landing page, google auth, fixed delete meeting

--- meeting_agent/urls.py
+++ meeting_agent/urls.py
@@ -3,29 +3,20 @@ URL configuration for meeting_agent project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
+from landing_page.views import landing_page  # Your landing page view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
-    # Allauth’s built‑in routes:
+    # Allauth’s built-in routes
     path('accounts/', include('allauth.urls')),
-
-    # Your custom login/signup app
+    # Your custom login/signup app under /login/
     path('login/', include('login_signup_app.urls')),
-
     # Meetings dashboard (requires login)
-     path('dashboard/', include('create_meeting_app.urls')),
+    path('dashboard/', include('create_meeting_app.urls')),
+    # Public landing page at root
+    path('', landing_page, name='landing_page'),
 ]
+
